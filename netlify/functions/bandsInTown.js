11d0ae3464e869a8f6c81632e7506a58b345f8c7
Karen Marting <karenannemarting@yahoo.com>
2021-09-13 18:19:19 -0700

    an experiment

---
+++ netlify/functions/bandsInTown.js
@@ -0,0 +1,16 @@
+
+const fetch = require('node-fetch')
+
+// exports.handler = async function () {
+//     const ticketmaster_API = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=C1ZnYwUXiCLmUx0Jy6P2Jji9m4n8AEui"
+//     const response = await fetch(ticketmaster_API)
+//     const data = await response.json()
+//         return {
+//             statusCode: 200,
+//             body: JSON.stringify(data)
+//         }
+
+//         const event = data.artist.items
+
+// }
+
